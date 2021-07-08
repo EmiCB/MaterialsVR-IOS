54ecdfe34729a24aadba0301f4a5a46beb9adf15
Emi Brown <emicb55@gmail.com>
2021-07-08 15:10:14 -0500

    Fixed Molecule Drifting Away From Player

--- MaterialsVRApp/Assets/Scripts/MoleculeController.cs
+++ MaterialsVRApp/Assets/Scripts/MoleculeController.cs
@@ -3,28 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// This class handles the behavior of each Molecule GameObject it is attached to.
+/// </summary>
 public class MoleculeController : MonoBehaviour {
-    SphereCollider currentMoleculeCollider;
-    EventTrigger currentEventTrigger;
+    private SphereCollider currentMoleculeCollider;
+    private EventTrigger currentEventTrigger;
 
-    public bool isSelected;
+    private Ray ray;
+    private Vector3 rayPoint;
+    private float distFromPlayer;
 
-    // Start is called before the first frame update
+    private bool isSelected;
+
     void Start() {
         isSelected = false;
-        getMoleculeComponents();
+        GetMoleculeComponents();
     }
 
-    // Update is called once per frame
     void Update() {
-        // handle dragging
+        // handle click and drag
         if (isSelected && Input.GetMouseButton(0)) {
-            dragMolecule();
+            DragMolecule();
         }
     }
 
-    // get componenets on current molecule
-    private void getMoleculeComponents() {
+    /// <summary>
+    /// Gets the necessary components of the molecule and adds them if they are missing.
+    /// </summary>
+    // TODO: rename this method as it is not a getter
+    // TODO: split up getting each component into its own method?
+    private void GetMoleculeComponents() {
         // set sphere collider or create a new one if it does not exist
         if (gameObject.GetComponent<SphereCollider>() == null) {
             currentMoleculeCollider = gameObject.AddComponent<SphereCollider>();
@@ -37,39 +46,64 @@ public class MoleculeController : MonoBehaviour {
         if (gameObject.GetComponent<EventTrigger>() == null) {
             currentEventTrigger = gameObject.AddComponent<EventTrigger>();
 
-            // set up a pointer down entry
+            // TODO: make less repetitive if possible
+            // set up a pointer enter entry
             EventTrigger.Entry pointerEnterEntry = new EventTrigger.Entry();
             pointerEnterEntry.eventID = EventTriggerType.PointerEnter;
             pointerEnterEntry.callback.AddListener((data) => { OnPointerEnter(); });
 
-            // set up a pointer down entry
+            // set up a pointer exit entry
             EventTrigger.Entry pointerExitEntry = new EventTrigger.Entry();
             pointerExitEntry.eventID = EventTriggerType.PointerExit;
             pointerExitEntry.callback.AddListener((data) => { OnPointerExit(); });
 
+            // set up a pointer click entry
+            EventTrigger.Entry pointerClickEntry = new EventTrigger.Entry();
+            pointerClickEntry.eventID = EventTriggerType.PointerClick;
+            pointerClickEntry.callback.AddListener((data) => { OnPointerClick(); });
+
             // add entries
             currentEventTrigger.triggers.Add(pointerEnterEntry);
             currentEventTrigger.triggers.Add(pointerExitEntry);
+            currentEventTrigger.triggers.Add(pointerClickEntry);
         } else {
             currentEventTrigger = gameObject.GetComponent<EventTrigger>();
         }
         
     }
 
+    /// <summary>
+    /// Runs when the player's pointer enters the molecule's collider.
+    /// </summary>
     public void OnPointerEnter() {
         isSelected = true;
     }
 
+    /// <summary>
+    /// Runs when the player's pointer exits the molecule's collider.
+    /// </summary>
     public void OnPointerExit() {
         isSelected = false;
     }
 
-    // move molecule to where player drags it
-    public void dragMolecule() {
-        // create a ray going from the camera through the mouse position
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+    /// <summary>
+    /// Runs when the player clicks while their pointer is over the molecule's collider.
+    /// </summary>
+    public void OnPointerClick() {
+        // ray going from the camera through the mouse position
+        ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
         // calculate the distance between the Camera and the GameObject, and go this distance along the ray
-        Vector3 rayPoint = ray.GetPoint(Vector3.Distance(transform.position, Camera.main.transform.position));
+        distFromPlayer = Vector3.Distance(transform.position, Camera.main.transform.position);
+    }
+
+    /// <summary>
+    /// Drags the molecule to wherever the player is looking.
+    /// </summary>
+    private void DragMolecule() {
+        // ray going from the camera through the mouse position
+        ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        // calculate the distance between the Camera and the GameObject, and go this distance along the ray
+        rayPoint = ray.GetPoint(distFromPlayer);
         // move the GameObject when you drag it
         transform.position = rayPoint;
     }
