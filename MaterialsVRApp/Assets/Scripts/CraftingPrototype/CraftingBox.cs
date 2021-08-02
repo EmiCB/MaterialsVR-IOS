dc7bcb176ea13c8e1dc763fcc74e918b7ec08463
Emi Brown <emicb55@gmail.com>
2021-08-02 18:16:28 -0500

    Added Crafting Box That Recognizes Objects That Enter And Exit It

---
+++ MaterialsVRApp/Assets/Scripts/CraftingPrototype/CraftingBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingBox : MonoBehaviour {
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        Debug.Log(collider.gameObject.name + " has entered the area");
+    }
+
+    void OnTriggerExit(Collider collider) {
+        Debug.Log(collider.gameObject.name + " has exited the area");
+    }
+}
---
+++ MaterialsVRApp/Assets/Scripts/CraftingPrototype/Sphere.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+
+    public bool toggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggle = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Toggle();
+        }
+    }
+
+    void Toggle() {
+        if (toggle) {
+            this.gameObject.transform.position = new Vector3(0, 0, 0);
+        } else {
+            this.gameObject.transform.position = new Vector3(-15, 0, 0);
+        }
+        toggle = !toggle;
+    }
+}
